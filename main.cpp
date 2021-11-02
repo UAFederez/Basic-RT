1ef94d2f1f6a00f3dc8e033b4edf5d5af1634c81
Uriel Federez <uafederez@gmail.com>
2021-11-03 01:53:57 +0800

    Initial implementation of threading

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@
 #include <thread>
 #include <cstring>
 #include <cfloat>
+#include <time.h>
+#include <sys/time.h>
 
 #include "util/ProgressBar.h"
 #include "util/BitmapImage.h"
+#include "util/Threading.h"
 
 #include "math/Vector3.h"
 #include "math/Ray.h"
@@ -37,12 +40,38 @@ Vector3 color(const Ray& r, const World& world, int depth)
     float t = 0.5 * (unit_dir.y() + 1.0f);
     return (1.0 - t) * Vector3(1.0, 1.0, 1.0) + t * Vector3(0.5, 0.7, 1.0);
 }
+
+void* process_thread_work(void* info_param)
+{
+    WorkerInfo* info = (WorkerInfo*) info_param;
+
+    for( uint32_t row = 0; row < info->image_height; row++ )
+    {
+        for( uint32_t col = 0; col < info->image_width; col++ )
+        {
+            Vector3 pixel_color = {};
+            for(uint32_t i = 0; i < info->num_samples; i++)
+            {
+                float u = float(col + float(rand())/float(RAND_MAX)) / float(info->image_width);
+                float v = float(row + float(rand())/float(RAND_MAX)) / float(info->image_height);
+
+                Ray r  = info->scene.camera->get_ray(u, v);
+                pixel_color += color(r, *info->scene.world, 0);
+            }
+            info->pixels.push_back(pixel_color);
+            info->finished_pixels++;
+        }
+    }
+    return NULL;
+}
+
 int main()
 {
+
     srand(time(0));
     const int IMAGE_WIDTH  = 1280;
     const int IMAGE_HEIGHT = 720;
-    const int NUM_SAMPLES  = 1;
+    const int NUM_SAMPLES  = 12;
     const float NS_DENOM   = 1 / float(NUM_SAMPLES);
 
     std::vector<uint8_t>   pixels;
@@ -107,37 +136,87 @@ int main()
     progress.height   = IMAGE_HEIGHT;
     progress.finished = 0;
 
-    ThreadInfo thread_info;
-    create_thread(&progress, &thread_info);
+    //ThreadInfo thread_info;
+    //create_thread(&progress, &thread_info);
+    
+    const uint32_t NUM_THREADS        = 12;
+    const uint32_t SAMPLES_PER_THREAD = (NUM_SAMPLES / NUM_THREADS);
+    WorkerInfo work_info[NUM_THREADS];
+    pthread_t  threads[NUM_THREADS];
 
-    for( int row = 0; row < IMAGE_HEIGHT; row++ )
+    printf("Each thread will do %d samples\n", SAMPLES_PER_THREAD);
+
+    for(uint32_t i = 0; i < NUM_THREADS; i++)
     {
-        for( int col = 0; col < IMAGE_WIDTH; col++ )
+        work_info[i].thread_id       = i;
+        work_info[i].num_samples     = SAMPLES_PER_THREAD;
+        work_info[i].image_width     = IMAGE_WIDTH;
+        work_info[i].image_height    = IMAGE_HEIGHT;
+        work_info[i].finished_pixels = 0;
+        work_info[i].scene.camera    = &main_camera;
+        work_info[i].scene.world     = &scene;
+
+        if(i == NUM_THREADS - 1)
+            work_info[i].num_samples += NUM_SAMPLES % NUM_THREADS;
+
+        if(pthread_create(&threads[i], NULL, process_thread_work, (void*)&work_info[i]))
+            printf("[ERROR] Could not create thread #%d\n", i);
+    }
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    char bar[80];
+    memset(bar, ' ', 80);
+    while(true)
+    {
+        uint32_t total_finished = 0;
+        uint32_t total_to_do    = 0;
+
+        for(const WorkerInfo& info : work_info)
         {
-            Vector3 pixel_color = {};
-            for(int i = 0; i < NUM_SAMPLES; i++)
-            {
-                float u = float(col + float(rand())/float(RAND_MAX)) / float(IMAGE_WIDTH);
-                float v = float(row + float(rand())/float(RAND_MAX)) / float(IMAGE_HEIGHT);
+            total_finished += info.finished_pixels;
+            total_to_do    += info.image_width * info.image_height;
+        }
 
-                Ray r  = main_camera.get_ray(u, v);
-                pixel_color += color(r, scene, 0);
-            }
-            pixel_color *= NS_DENOM;
-            pixel_color  = Vector3(sqrt(pixel_color.x()), sqrt(pixel_color.y()), sqrt(pixel_color.z()));
+        float pct = float(total_finished) / float(total_to_do);
+        int nbars = pct * 80;
 
-            uint8_t image_red   = uint8_t(255.99 * pixel_color.x());
-            uint8_t image_green = uint8_t(255.99 * pixel_color.y());
-            uint8_t image_blue  = uint8_t(255.99 * pixel_color.z());
+        memset(bar, '#', nbars);
+        printf("[%s] [%.2f]\r", bar, pct * 100.0f);
 
-            pixels.push_back(image_blue);
-            pixels.push_back(image_green);
-            pixels.push_back(image_red);
-            progress.finished++;
+        if(pct == 1.0)
+        {
+            printf("\n");
+            break;
         }
     }
 
-    join_thread(&thread_info);
+    for(uint32_t i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+
+    std::vector<Vector3> total_pixels(IMAGE_WIDTH * IMAGE_HEIGHT);
+    for(const WorkerInfo& info : work_info)
+    {
+        for(uint32_t i = 0; i < info.pixels.size(); i++)
+           total_pixels[i] += info.pixels[i] * NS_DENOM;
+    }
+
+    for(Vector3& pixel : total_pixels)
+    {
+        pixel = Vector3(sqrt(pixel.x()), sqrt(pixel.y()), sqrt(pixel.z()));
+        pixels.push_back(uint8_t(255.99 * pixel.z()));
+        pixels.push_back(uint8_t(255.99 * pixel.y()));
+        pixels.push_back(uint8_t(255.99 * pixel.x()));
+    }
+    gettimeofday(&end, NULL);
+
+    double time_taken = end.tv_sec + end.tv_usec / 1e6 -
+                        start.tv_sec - start.tv_usec / 1e6; // in seconds
+
+    printf("time program took %f seconds to execute\n", time_taken);
+
+    //join_thread(&thread_info);
 
     write_bmp_to_file("output.bmp", pixels.data(), IMAGE_WIDTH, IMAGE_HEIGHT, 3);
     // Cleanup
---
+++ util/Threading.h
@@ -0,0 +1,27 @@
+#ifndef UTIL_THREADING_H
+#define UTIL_THREADING_H
+
+#include "../graphics/World.h"
+#include "../graphics/Camera.h"
+
+#include "../math/Vector3.h"
+
+struct SceneInfo
+{
+    Camera* camera;
+    World*  world;
+};
+
+struct WorkerInfo
+{
+    uint32_t thread_id;
+    uint32_t num_samples;
+    uint32_t image_width;
+    uint32_t image_height;
+    uint32_t finished_pixels;
+    std::vector<Vector3> pixels;
+
+    SceneInfo  scene;
+};
+
+#endif
