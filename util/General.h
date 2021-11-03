5beda87dc0c8615a35a66ca4b0daea7c8cfdddf6
Uriel Federez <uafederez@gmail.com>
2021-11-03 19:22:47 +0800

    Updated RNG

--- util/General.h
+++ util/General.h
@@ -1,12 +1,13 @@
 #ifndef UTIL_GENERAL_H
 #define UTIL_GENERAL_H
 
+#include <chrono>
+
 float random_float(const float min = 0.0, const float max = 1.0)
 {
-    static thread_local std::random_device device;
-    static thread_local std::mt19937 generator(device());
+    static std::mt19937 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
     std::uniform_real_distribution<float> distribution(min, max);
-    return distribution(generator);
+    return distribution(rng);
 }
 
 #endif
