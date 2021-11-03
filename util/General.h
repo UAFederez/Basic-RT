74bb40962c0f666d3b7d1aa114c131e64c481424
Uriel Federez <uafederez@gmail.com>
2021-11-03 12:00:05 +0800

    Changed to thread-safe RNG

---
+++ util/General.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_GENERAL_H
+#define UTIL_GENERAL_H
+
+float random_float(const float min = 0.0, const float max = 1.0)
+{
+    static thread_local std::mt19937 generator;
+    std::uniform_real_distribution<float> distribution(min, max);
+    return distribution(generator);
+}
+
+#endif
