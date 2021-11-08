fa3c625e996b9604d03646aa550d92fb77eaeec2
Uriel Federez <uafederez@gmail.com>
2021-11-08 14:52:25 +0800

    Minor optimizations on intersection check

--- graphics/Triangle.cpp
+++ graphics/Triangle.cpp
@@ -6,42 +6,41 @@ bool Triangle::hit(const Ray& r,
                    HitRecord& rec) const
 {
     // Face normal
-    Vec3 normal = normalize(cross(B - A, C - A));
+    Vec3 normal = cross(B - A, C - A);
 
     // Check if the point is within the supporting plane
-    const float denom = -dot(normal, r.direction());
+    const double denom = -dot(normal, r.direction());
 
-    // TODO: maybe add option for single vs. double-sided
-    // fabs to allow viewing from both sides
-    //
-    if(denom < 1e-3 && !material->is_double_sided)
+    if(denom < 1e-6 && !material->is_double_sided)
         return false;
 
-    const float t = dot(normal, r.origin() - A) / denom;
+    const double t = dot(normal, r.origin() - A) / denom;
 
     if(t_min > t || t > t_max)
         return false;
 
     // Outside-inside test
-    Vec3  Q = r.point_at_t(t);
-    float CAB  = dot(cross((B - A), (Q - A)), normal);
-    float CCB  = dot(cross((C - B), (Q - B)), normal);
-    float CAC  = dot(cross((A - C), (Q - C)), normal);
+    const Vec3  Q    = r.point_at_t(t);
+    const double CAB = dot(cross((B - A), (Q - A)), normal);
+    const double CCB = dot(cross((C - B), (Q - B)), normal);
+    const double CAC = dot(cross((A - C), (Q - C)), normal);
 
     if(CAB < 0 || CCB < 0 || CAC < 0)
         return false;
 
-    float area = dot(cross((B - A), (C - A)), normal);
+    // Changed from cross(B - A, C - A) because it is the same
+    const double area  = dot(normal, normal);
+    const double darea = 1.0 / area;
 
-    float alpha = CCB / area;
-    float beta  = CAC / area;
-    float gamma = CAB / area;
+    const double alpha = CCB * darea;
+    const double beta  = CAC * darea;
+    const double gamma = (1 - alpha - beta);
 
     // If vertex normals have been explicitly defined
     if(a_nrm.magnitude_squared() != 0 &&
        b_nrm.magnitude_squared() != 0 &&
        c_nrm.magnitude_squared() != 0 )
-        normal = normalize((a_nrm * alpha) + (b_nrm * beta) + (c_nrm * gamma));
+        normal = (a_nrm * alpha) + (b_nrm * beta) + (c_nrm * gamma);
 
     if(denom < 0 && material->is_double_sided)
         normal = -normal;
@@ -49,7 +48,7 @@ bool Triangle::hit(const Ray& r,
     rec.uv           = Vec2({ beta, gamma });
     rec.t            = t;
     rec.point_at_t   = r.point_at_t(rec.t);
-    rec.normal       = normal;
+    rec.normal       = normalize(normal);
     rec.material_ptr = material;
     return true;
 }
@@ -64,10 +63,10 @@ BoundsDefinition Triangle::get_bounds() const
     for(const Vec3 v : vertices)
     {
         for(int i = 0; i < 3; i++)
+        {
             low_far[i] = std::min(low_far[i], v[i]);
-
-        for(int i = 0; i < 3; i++)
             up_near[i] = std::max(up_near[i], v[i]);
+        }
     }
 
     return BoundsDefinition { low_far, up_near };
